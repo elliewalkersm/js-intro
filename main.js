8a41444cf88173ed944f85915d41f9c9c74e8ad9
elliewalkersm <elliewalkersm@gmail.com>
2021-01-16 11:07:58 -0600

    Variables, data types, and math

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("It worked!");
+// console.log("It worked!");
+
+// *****VARIABLES******
+// let firstName = 'Ed';
+// let lastName = 'Sheeran';
+
+// console.log(`${firstName} ${lastName}`);
+
+// firstName = 'Ellie';
+// lastName = 'Walker';
+// console.log(`${firstName} ${lastName}`);
+
+// const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+
+// *****DATA TYPES******
+
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 17;
+// const negative = -23;
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavaScript = true;
+// console.log(typeof iLoveJavaScript);
+
+// *****MATH******
+
+// console.log(Math.pow(2,2)); //4
+// console.log(Math.pow(3,2)); //9
+// console.log(Math.pow(3,3)); //27
+
+// console.log(Math.round(6.5)); //7
+// console.log(Math.round(6.45)); //6
+// console.log(Math.floor(6.9)); //6
+// console.log(Math.ceil(6.001)); //7
+
+const cat = 'kitty'
+console.log(cat.length);
