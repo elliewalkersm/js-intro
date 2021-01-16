1a7ca3ed8276e7ebd850dc532a145d28d173d60e
elliewalkersm <elliewalkersm@gmail.com>
2021-01-16 11:58:08 -0600

    Functions with and without arguments

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("It worked!");
+// // console.log("It worked!");
+
+//****FUNCTIONS*****/
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+// sayHello();
+
+// const sayHello = function() {
+//   console.log("Hello");
+// }
+
+// const sayHello = () => {
+//   console.log("Hello");
+// }
+
+// sayHello();
+
+// *****FUNCTIONS WITH ARGUMENTS****
+
+// function logsHello(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// logsHello('Ellie');
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2; 
+  console.log(sum);
+  return sum;
+}
+
+addsTwoNumbers(6, 6);
+console.log(addsTwoNumbers(6, 6));
